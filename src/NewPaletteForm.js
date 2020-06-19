aa90922c888458e04da0b7cd99e0c81094f84be4
tran.thu.trangb <tran.thu.trangb@sun-asterisk.com>
2020-06-19 15:22:09 +0700

    done emoji picker

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,74 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import PaletteFormNav from './PaletteFormNav';
-import ColorPickerForm from './ColorPickerForm';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import DraggableColorList from './DraggableColorList';
-import { arrayMove } from 'react-sortable-hoc';
+import React, { useState, useEffect } from "react";
+import PaletteFormNav from "./PaletteFormNav";
+import ColorPickerForm from "./ColorPickerForm";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import DraggableColorList from "./DraggableColorList";
+import { arrayMove } from "react-sortable-hoc";
 
 const drawerWidth = 400;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   hide: {
-    display: 'none',
+    display: "none"
   },
   drawer: {
     width: drawerWidth,
-    flexShrink: 0,
+    flexShrink: 0
   },
   drawerPaper: {
     width: drawerWidth,
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center"
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end"
   },
   content: {
     flexGrow: 1,
-    height: 'calc(100vh - 64px)',
+    height: "calc(100vh - 64px)",
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
-    marginLeft: -drawerWidth,
+    marginLeft: -drawerWidth
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
+      duration: theme.transitions.duration.enteringScreen
     }),
-    marginLeft: 0,
+    marginLeft: 0
   },
   container: {
-    width: '90%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%'
+    width: "90%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%"
   },
   buttons: {
-    width: '100%',
+    width: "100%"
   },
   button: {
-    width: '50%'
+    width: "50%"
   }
 }));
 
@@ -86,7 +86,7 @@ export default function PersistentDrawerLeft(props) {
     setOpen(false);
   };
 
-  const addColor = (newColor) => {
+  const addColor = newColor => {
     setColors([...colors, newColor]);
     //setNewColorName('');
   };
@@ -95,19 +95,17 @@ export default function PersistentDrawerLeft(props) {
   //   setNewPaletteName(e.target.value);
   // };
 
-  const savePalette = (palettename) => {
-    //let name = newPaletteName;
-    const newPalette = {
-      id: palettename.toLowerCase().replace(/ /g, '-'),
-      paletteName: palettename,
-      colors: colors,
-    };
-    props.savePalette(newPalette);
-    props.history.push('/');
+  const savePalette = finalPalette => {
+    props.savePalette({
+      ...finalPalette,
+      id: finalPalette.paletteName.toLowerCase().replace(/ /g, "-"),
+      colors
+    });
+    props.history.push("/");
   };
 
-  const deleteColor = (colorName) => {
-    setColors(colors.filter((color) => color.name !== colorName));
+  const deleteColor = colorName => {
+    setColors(colors.filter(color => color.name !== colorName));
   };
 
   const onSortEnd = ({ oldIndex, newIndex }) => {
@@ -119,7 +117,7 @@ export default function PersistentDrawerLeft(props) {
   };
 
   const addRandomColor = () => {
-    const allColors = props.palettes.map((palette) => palette.colors).flat();
+    const allColors = props.palettes.map(palette => palette.colors).flat();
     var randomIndex = Math.floor(Math.random() * allColors.length);
     const randomColor = allColors[randomIndex];
     setColors([...colors, randomColor]);
@@ -141,12 +139,12 @@ export default function PersistentDrawerLeft(props) {
         anchor='left'
         open={open}
         classes={{
-          paper: classes.drawerPaper,
+          paper: classes.drawerPaper
         }}
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? (
+            {theme.direction === "ltr" ? (
               <ChevronLeftIcon />
             ) : (
               <ChevronRightIcon />
@@ -155,31 +153,38 @@ export default function PersistentDrawerLeft(props) {
         </div>
         <Divider />
         <div className={classes.container}>
-        <Typography variant='h4' gutterBottom>Trang's New Palette</Typography>
-        <div className={classes.buttons}>
-          <Button variant='contained' color='secondary' onClick={clearColor} className={classes.button}>
-            Clear Palette
-          </Button>
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={addRandomColor}
-            disabled={paletteFull}
-            className={classes.button}
-          >
-            Random Color
-          </Button>
-        </div>
-        <ColorPickerForm
-          paletteFull={paletteFull}
-          addColor={addColor}
-          colors={colors}
-        />
+          <Typography variant='h4' gutterBottom>
+            Trang's New Palette
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant='contained'
+              color='secondary'
+              onClick={clearColor}
+              className={classes.button}
+            >
+              Clear Palette
+            </Button>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={addRandomColor}
+              disabled={paletteFull}
+              className={classes.button}
+            >
+              Random Color
+            </Button>
+          </div>
+          <ColorPickerForm
+            paletteFull={paletteFull}
+            addColor={addColor}
+            colors={colors}
+          />
         </div>
       </Drawer>
       <main
         className={clsx(classes.content, {
-          [classes.contentShift]: open,
+          [classes.contentShift]: open
         })}
       >
         <div className={classes.drawerHeader} />
@@ -195,5 +200,5 @@ export default function PersistentDrawerLeft(props) {
 }
 
 PersistentDrawerLeft.defaultProps = {
-  maxColors: 20,
+  maxColors: 20
 };
--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -11,20 +11,16 @@ import { Picker } from "emoji-mart";
 import "emoji-mart/css/emoji-mart.css";
 
 const PaletteMetaForm = props => {
-  const [open, setOpen] = React.useState(true);
+  const [stage, setStage] = React.useState("form");
   const [newPaletteName, setNewPaletteName] = useState("");
   const { palettes, savePalette, hideForm } = props;
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   const handleNewPaletteNameChange = e => {
     setNewPaletteName(e.target.value);
+  };
+
+  const showEmojiPicker = () => {
+    setStage("emoji");
   };
 
   useEffect(() => {
@@ -35,36 +31,52 @@ const PaletteMetaForm = props => {
     });
   }, []);
 
+  const saveFinalPalette = emojiObject => {
+    savePalette({
+      paletteName: newPaletteName,
+      emoji: emojiObject.native
+    });
+  };
+
   return (
-    <Dialog open={open} onClose={hideForm} aria-labelledby='form-dialog-title'>
-      <DialogTitle id='form-dialog-title'>Choose A Palette Name</DialogTitle>
-      <ValidatorForm onSubmit={() => savePalette(newPaletteName)}>
-        <DialogContent>
-          <DialogContentText>
-            Enter a palette name. Make sure it is unique.
-          </DialogContentText>
-          <Picker />
-          <TextValidator
-            value={newPaletteName}
-            label='Palette Name'
-            name='newPaletteName'
-            fullWidth
-            margin='normal'
-            onChange={handleNewPaletteNameChange}
-            validators={["required", "isPaletteNameUnique"]}
-            errorMessages={["Enter Palette Name", "Name already taken"]}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={hideForm} color='primary'>
-            Cancel
-          </Button>
-          <Button variant='contained' color='primary' type='submit'>
-            Save Palette
-          </Button>
-        </DialogActions>
-      </ValidatorForm>
-    </Dialog>
+    <div>
+      <Dialog open={stage === "emoji"} onClose={hideForm}>
+        <DialogTitle id='form-dialog-title'>Pick an Emoji</DialogTitle>
+        <Picker onSelect={saveFinalPalette} title='Pick palette emoji' />
+      </Dialog>
+      <Dialog
+        open={stage === "form"}
+        onClose={hideForm}
+        aria-labelledby='form-dialog-title'
+      >
+        <DialogTitle id='form-dialog-title'>Choose A Palette Name</DialogTitle>
+        <ValidatorForm onSubmit={showEmojiPicker}>
+          <DialogContent>
+            <DialogContentText>
+              Enter a palette name. Make sure it is unique.
+            </DialogContentText>
+            <TextValidator
+              value={newPaletteName}
+              label='Palette Name'
+              name='newPaletteName'
+              fullWidth
+              margin='normal'
+              onChange={handleNewPaletteNameChange}
+              validators={["required", "isPaletteNameUnique"]}
+              errorMessages={["Enter Palette Name", "Name already taken"]}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={hideForm} color='primary'>
+              Cancel
+            </Button>
+            <Button variant='contained' color='primary' type='submit'>
+              Save Palette
+            </Button>
+          </DialogActions>
+        </ValidatorForm>
+      </Dialog>
+    </div>
   );
 };
 
