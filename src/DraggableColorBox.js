aaddf9bd652488de0aadd7afd6cf40509677751c
tran.thu.trangb <tran.thu.trangb@sun-asterisk.com>
2020-06-19 15:40:10 +0700

    move JSS styles out

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,40 +1,10 @@
-import React from 'react';
-import { withStyles } from '@material-ui/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { SortableElement } from 'react-sortable-hoc';
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { SortableElement } from "react-sortable-hoc";
+import styles from "./styles/DraggableColorBoxStyles";
 
-const styles = {
-  root: {
-    width: '20%',
-    height: '25%',
-    display: 'inline-block',
-    position: 'relative',
-    cursor: 'pointer',
-    marginBottom: '-3.5px',
-    '&:hover svg': {
-      color: 'white',
-      transform: 'scale(1.5)',
-    },
-  },
-  boxContent: {
-    position: 'absolute',
-    width: '100%',
-    padding: '10px',
-    left: '0px',
-    bottom: '0px',
-    color: 'rgba(0, 0, 0, 0.5)',
-    letterSpacing: '1px',
-    textTransform: 'uppercase',
-    fontSize: '12px',
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  deleteIcon: {
-    transition: 'all 0.3s ease-in-out',
-  },
-};
-
-const DraggableColorBox = SortableElement((props) => {
+const DraggableColorBox = SortableElement(props => {
   const { classes, name, handleClick, color } = props;
   return (
     <div className={classes.root} style={{ background: color }}>
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PaletteFormNav from "./PaletteFormNav";
 import ColorPickerForm from "./ColorPickerForm";
 import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
@@ -12,73 +12,13 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import DraggableColorList from "./DraggableColorList";
 import { arrayMove } from "react-sortable-hoc";
-
-const drawerWidth = 400;
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    display: "flex",
-    alignItems: "center"
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  content: {
-    flexGrow: 1,
-    height: "calc(100vh - 64px)",
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
-  container: {
-    width: "90%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%"
-  },
-  buttons: {
-    width: "100%"
-  },
-  button: {
-    width: "50%"
-  }
-}));
+import useStyles from "./styles/NewPaletteFormStyle";
 
 export default function PersistentDrawerLeft(props) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = useState(true);
   const [colors, setColors] = useState(props.palettes[0].colors);
-  //const [newColorName, setNewColorName] = useState('');
-  //const [newPaletteName, setNewPaletteName] = useState('');
   const { palettes, maxColors } = props;
   const paletteFull = colors.length >= maxColors;
 
@@ -88,12 +28,7 @@ export default function PersistentDrawerLeft(props) {
 
   const addColor = newColor => {
     setColors([...colors, newColor]);
-    //setNewColorName('');
   };
-
-  // const handleNewPaletteNameChange = (e) => {
-  //   setNewPaletteName(e.target.value);
-  // };
 
   const savePalette = finalPalette => {
     props.savePalette({
