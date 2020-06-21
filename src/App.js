83c507ee4fdfbbf227dc43500d0c9ef2fb6b9658
Tran Thu Trang <tranthutrang@Trans-MacBook-Pro.local>
2020-06-21 21:54:08 +0700

    prevent duplicate random color

--- src/App.js
+++ src/App.js
@@ -93,6 +93,17 @@ function App() {
                   </Page>
                 )}
               />
+              <Route
+                render={(routeProps) => (
+                  <Page>
+                    <PaletteList
+                      palettes={palettes}
+                      deletePalette={deletePalette}
+                      {...routeProps}
+                    />
+                  </Page>
+                )}
+              />
             </Switch>
           </CSSTransition>
         </TransitionGroup>
